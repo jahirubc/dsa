2ef4d1742c878efea5cb9b8b192dd3be72718e50
Md. Jahir Uddin Babar <jahirubc@gmail.com>
2024-12-26 16:31:07 +0600

    Binary Search Completed

--- src/Search_Linear.java
+++ src/Search_Linear.java
@@ -1,7 +1,7 @@
 public class Search_Linear {
     public static void main(String[] args) {
         int[] nums = {5, 7, 9, 11, 13};
-        int target = 11;
+        int target = 5;
 
         int result = linearSearch(nums, target);
         if (result != -1)
